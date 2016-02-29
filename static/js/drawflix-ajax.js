7768f68a6af1d5d243f3b86c232aba4beb7844cf
David McLintock <damclintock@gmail.com>
2016-02-29 22:00:32 +0000

    Clear search box/additional .js comments

--- static/js/drawflix-ajax.js
+++ static/js/drawflix-ajax.js
@@ -1,20 +1,21 @@
 $(document).ready( function() {
-	
+
+	//when submit_search button clicked
 	$('#submit_search').click(function(event){
-		var searchFilm = $("#film_search").val();
-				
+		var searchFilm = $("#film_search").val(); //value to search for is value in searchFilm box
+
+		//AJAX request to OMDB API
     $.getJSON('http://www.omdbapi.com/?s=' + searchFilm, function(json_data){
-		$('#options option').remove();
-		for (var i = 0; i < json_data.Search.length; i++) {
+		$('#options option').remove(); //remove any previous options on the dropdown
+		for (var i = 0; i < json_data.Search.length; i++) { //loop through JSON results and append to options
 		$('#options').append('<option value="' + json_data.Search[i].Title + '">' + json_data.Search[i].Title + '</option>');
 		}
     });
-	
+	//unhide the options box if hidden(default)
 	$("#options").show();
+	//clear the search box for next input
+	$("#film_search").val('');
 });
-	
-	
-});			
 
 
-
+});
