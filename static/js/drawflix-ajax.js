52cca57218f74557d1500b59a9b82ba173eb8a3d
David McLintock <damclintock@gmail.com>
2016-03-10 23:17:48 +0000

    Made film search non-async/first dropdown defaults to selected film

--- static/js/drawflix-ajax.js
+++ static/js/drawflix-ajax.js
@@ -6,20 +6,29 @@ $(document).ready( function() {
 
 	//when submit_search button clicked
 	$('#submit_search').click(function(event){
-		var searchFilm = $("#film_search").val(); //value to search for is value in searchFilm box
-
-		//AJAX request to OMDB API
-    $.getJSON('http://www.omdbapi.com/?s=' + searchFilm, function(json_data){
+		var searchFilm =  $("#film_search").val(); //value to search for is value in searchFilm box
+		$.ajax({
+			type: 'GET',
+  url: 'http://www.omdbapi.com/?s=' + searchFilm,
+  dataType: 'json',
+  async: false,
+  success: function(json_data){
 		$('#options option').remove(); //remove any previous options on the dropdown
 		for (var i = 0; i < json_data.Search.length; i++) { //loop through JSON results and append to options
 		$('#options').append('<option value="' + json_data.Search[i].Title + '">' + json_data.Search[i].Title + '</option>');
 		}
-    });
-	//unhide the options box if hidden(default)
-	$("#options").show();
-	//clear the search box for next input
-	$("#film_search").val('');
-});
+		//unhide the options box if hidden(default)
+		$("#options").show();
+		//clear the search box for next input
+		$("#film_search").val('');
+		//auto selects first film in dropdown if first result of search is desired user response
+		$("#id_film").val($("#options").val());
+	}
+		});
+	});
+
+
+
 
 $("#options").on("change", function() {
       $("#id_film").val($(this).val());
@@ -50,9 +59,9 @@ $("#options").on("change", function() {
 });
 
 	$("#Test").click(function() {
-		
+
 		var canvas = myBoard.getImg();
-		
+
 		$("#id_image").val(canvas);
 
 
