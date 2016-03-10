9bd90c17ffb0e7f573636af6f9d2d06260f409c7
David McLintock <damclintock@gmail.com>
2016-03-10 22:29:21 +0000

    Templates updated to display Anonymous if relevant

--- drawflix/admin.py
+++ drawflix/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from drawflix.models import Drawing, Like, UserProfile
+from drawflix.models import Drawing, Like
 
 
 class FilmAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(UserProfile)
+
 admin.site.register(Drawing)
 admin.site.register(Like)
 
--- drawflix/models.py
+++ drawflix/models.py
@@ -5,18 +5,20 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 
-class UserProfile(models.Model):
-    # This line is required. Links UserProfile to a User model instance.
-    user = models.OneToOneField(User)
-
-
-    # The additional attributes we wish to include.
-    website = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
-
-    # Override the __unicode__() method to return out something meaningful!
-    def __unicode__(self):
-        return self.user.username
+# class UserProfile(models.Model):
+#     # This line is required. Links UserProfile to a User model instance.
+#     user = models.OneToOneField(User)
+#
+#
+#     # The additional attributes we wish to include.
+#     website = models.URLField(blank=True)
+#     picture = models.ImageField(upload_to='profile_images', blank=True)
+#
+#     # Override the __unicode__() method to return out something meaningful!
+#     def __unicode__(self):
+#         if self.user.is_anonymous():
+#             return "Anonymous"
+#         return "fucksticks"
 
 # class Film(models.Model):
 #     title = models.CharField(max_length=128, unique=True)
