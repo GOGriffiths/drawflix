f24fbcc794c04f2c14b01cee717da8511cb6b6df
David McLintock <damclintock@gmail.com>
2016-03-22 18:49:11 +0000

    Separated jquery from ajax

--- static/js/drawflix-ajax.js
+++ static/js/drawflix-ajax.js
@@ -1,8 +1,4 @@
-
-
 $(document).ready( function() {
-
-
 
 	//when find film button clicked
 	$('#submit_search').click(function(event){
@@ -11,7 +7,7 @@ $(document).ready( function() {
 			type: 'GET',
   url: 'http://www.omdbapi.com/?s=' + searchFilm,
   dataType: 'json',
-  async: false,
+  async: false, //wait for response before updating details
   success: function(json_data){
 		$('#options option').remove(); //remove any previous options on the dropdown
 		for (var i = 0; i < json_data.Search.length; i++) { //loop through JSON results and append to options
@@ -28,67 +24,5 @@ $(document).ready( function() {
 	}
 		});
 	});
-
-//adds selected film to hidden submission form
-$("#options").on("change", function() {
-      $("#id_film").val($(this).val());
-    });
-
-
-
-	//for likes ----------
-	$(document).on("click","#like", function(){
-		var catid;
-		catid = $(this).attr("data-catid");
-		$.get('/like_drawing/', {drawing_id: catid}, function(data){
-			$('#' + catid).html(data);
-
-    });
-
-	});
-
-//creates drawing board object
-	var myBoard = new DrawingBoard.Board('test_board',{
-	controls: [
-		'Color',
-		{ Size: { type: 'dropdown' } },
-		{ DrawingMode: { filler: true } },
-		'Navigation',
-	],
-	size: 1,
-	webStorage: 'local',
-	enlargeYourContainer: true
-});
-
-//when user submits drawing
-	$("#finalise").click(function() {
-
-		film = $("#id_film").val();
-		//if film has been selected
-		if(film != '') {
-
-		var canvas = myBoard.getImg();
-
-		$("#id_image").val(canvas);
-		$('#confirm_drawing').click()
-		$('.drawing-board-control-navigation-reset').click();
-}
-else { //if not, display error message
-	$("#submit_error").fadeIn(500);
-}
-
-});
-
-//fades in content
-$("#fade_in").fadeIn(1000);
-
-//highlights current page on navbar
-$(function(){
-  $('a').each(function() {
-    if ($(this).prop('href') == window.location.href) {
-      $(this).addClass('highlights');
-    }
-  });
-});
 
 });
